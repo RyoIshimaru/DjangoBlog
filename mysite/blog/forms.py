dea68428e3d33367ae818b4b1bfbd25528831b21
RyoIshimaru <ryo.ishimaru.kyoto@gmail.com>
2020-05-19 06:22:14 +0900

    Created all view and url

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,10 +1,10 @@
-import forms
+from django import forms
 from blog.models import Post, Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        field = ('author', 'title', 'text')
+        fields = ('author', 'title', 'text')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'textinputclass'}),
@@ -15,7 +15,7 @@ class PostForm(forms.ModelForm):
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        field = ('author', 'text')
+        fields = ('author', 'text')
 
         widgets = {
             'author': forms.TextInput(attrs={'class': 'textinputclass'}),
